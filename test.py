22c39c0a71acc3545409a6b87ea9ee46d0698d8c
Demomesh <Ademola.mesele@gmail.com>
2023-03-14 10:52:58 +0100

    updated code with a an additional functionality

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 def github_learning():
-    Print('Welcome to github')
+      # Add two numbers
+      Var1 = 5 
+      Var2 = 10
+      sum_of_num = Var1 + Var2
+      print(f'The sum of the numbers is {sum_of_num}')
+         github_learning()
